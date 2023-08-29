d78e4ae1ac7f46bfd04bd612734ba51bf2ca2349
Owolabi Oyekanmi <ttconfirmed@gmail.com>
2023-08-29 12:39:49 +0100

    updated the client server grpc arithmetic program

---
+++ calculator_client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/ojzene/goarithmetic/calculator/calculatorpb"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+	// conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := calculatorpb.NewCalculatorServiceClient(conn)
+	expression := "10 + 5"
+	req := &calculatorpb.ExpressionRequest{Expression: expression}
+
+	resp, err := client.EvaluateExpression(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Failed to evaluate expression: %v", err)
+	}
+
+	fmt.Printf("Result of '%s' is %d\n", expression, resp.Result)
+}
---
+++ calculator_server/server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/ojzene/goarithmetic/calculator/calculatorpb"
+	"google.golang.org/grpc/reflection"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct{}
+
+func (s *server) EvaluateExpression(ctx context.Context, req *calculatorpb.ExpressionRequest) (*calculatorpb.ExpressionResponse, error) {
+	expression := req.Expression
+	result := evaluateExpression(expression)
+	return &calculatorpb.ExpressionResponse{Result: result}, nil
+}
+
+func evaluateExpression(expression string) int32 {
+	var num1, num2 int32
+	operator := ""
+	fmt.Sscanf(expression, "%d %s %d", &num1, &operator, &num2)
+
+	switch operator {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		if num2 != 0 {
+			return num1 / num2
+		} else {
+			log.Println("Division by zero")
+		}
+	}
+
+	return 0
+}
+
+func main() {
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	reflection.Register(s)
+
+	log.Println("Server started at :50051")
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
